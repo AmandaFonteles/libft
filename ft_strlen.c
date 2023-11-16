e381ab491d66d7d24650ba5ab0560adf7f5042e9
afontele <107762302+AmandaFonteles@users.noreply.github.com>
2023-11-16 13:38:50 +0000

    ft_strlen incomplete

---
+++ ft_strlen.c
@@ -0,0 +1,21 @@
+//COLOCAR HEADER 42
+
+#include <unistd.h>
+#include <stdio.h>
+
+size_t ft_strlen(const char *str) //we use a const to indicate that the string shouldn't be modified by the function
+{
+    size_t i; //size_t is an unsigned integer type. Here we have a size_t variable named i.
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+int main (void)
+{
+ TESTAAAAAR
+}
