42a789c77edcdacc2a1abc2435eca542235ae7f6
afontele <107762302+AmandaFonteles@users.noreply.github.com>
2023-12-03 00:08:37 +0000

    11th push

---
+++ ft_atoi.c
@@ -0,0 +1,63 @@
+//HEADER 42
+
+#include <unistd.h>
+//#include <MINHA BIBLIOTECA>
+
+int ft_atoi(const char  *str) //converts the INITIAL PORTION of the string pointed to by nptr to int
+{
+	int neg;
+	int i;
+	int num;
+
+	i = 0;
+	neg = 1;
+	num = 0;
+	while (str[i] == 32 || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
+			|| str[i] == '\f' || str[i] == '\r')
+    {
+		i++;
+    }
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (num * neg);
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>  // For the standard atoi function
+
+int ft_atoi(const char *str);  // Assuming your ft_atoi is defined elsewhere
+
+int main() {
+    char *test_strs[] = {
+        "   -123",      // Leading spaces with a negative number
+        "42",           // Simple positive number
+        "+0",           // Plus sign with zero
+        "   +456",      // Leading spaces with a positive number
+        "-2147483648",  // Integer minimum
+        "2147483647",   // Integer maximum
+        "not a number", // Non-numeric string
+        "9876543210",   // Larger than int max, potential overflow
+        "-9999999999"   // Smaller than int min, potential underflow
+    };
+
+    int num_tests = sizeof(test_strs) / sizeof(test_strs[0]);
+
+    for (int i = 0; i < num_tests; i++) {
+        int std_atoi_result = atoi(test_strs[i]);
+        int ft_atoi_result = ft_atoi(test_strs[i]);
+        printf("Test %d - String: '%s'\n", i + 1, test_strs[i]);
+        printf("Standard atoi: %d\n", std_atoi_result);
+        printf("ft_atoi: %d\n\n", ft_atoi_result);
+    }
+
+    return 0;
+}*/
---
+++ ft_calloc.c
@@ -0,0 +1,17 @@
+//HEADER 42
+
+#include <unistd.h>
+//#include <minha biblioteca>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *ptr;
+    size_t  total_size;
+
+    total_size = size * count;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, total_size);
+	return (ptr);
+}
