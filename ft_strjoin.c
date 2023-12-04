fa23f2775a313f5106c5eac4762faf1b8dc38534
afontele <107762302+AmandaFonteles@users.noreply.github.com>
2023-12-04 15:12:13 +0000

    12th push

---
+++ ft_strjoin.c
@@ -0,0 +1,87 @@
+//HEADER 42
+
+#include <unistd.h>
+//#include minha bibli
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char    *ft_strjoin(const char  *s1, const char *s2) 
+{
+    char    *result;
+    size_t  len_s1;
+    size_t  len_s2;
+    size_t  i;
+    size_t  j;
+
+    len_s1 = 0;
+    len_s2 = 0;
+    i = 0;
+    j = 0;
+
+    if (s1 != NULL)
+        len_s1 = strlen(s1);
+    if (s2 != NULL)
+        len_s2 = strlen(s2);
+    
+    result = (char*)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+    if (!result)
+        return NULL;
+    
+    while (s1 && s1[i]) {
+        result[i] = s1[i];
+        i++;
+    }
+    while (s2 && s2[j]) {
+        result[i + j] = s2[j];
+        j++;
+    }
+    result[i + j] = '\0';
+    return result;
+}
+
+/*// Sample main function for testing
+int main() {
+    char *result;
+    char *test1 = "Hello, ";
+    char *test2 = "World!";
+    char *empty = "";
+    char *null = NULL;
+
+    // Test with normal strings
+    result = ft_strjoin(test1, test2);
+    printf("Joining 'Hello, ' and 'World!': %s\n", result);
+    free(result); // Remember to free the memory
+
+    // Test with one empty string
+    result = ft_strjoin(test1, empty);
+    printf("Joining 'Hello, ' and '': %s\n", result);
+    free(result);
+
+    // Test with other empty string
+    result = ft_strjoin(empty, test2);
+    printf("Joining '' and 'World!': %s\n", result);
+    free(result);
+
+    // Test with both strings empty
+    result = ft_strjoin(empty, empty);
+    printf("Joining '' and '': %s\n", result);
+    free(result);
+
+    // Test with one NULL string
+    result = ft_strjoin(test1, null);
+    printf("Joining 'Hello, ' and NULL: %s\n", result); // Your implementation may vary here
+    if (result) free(result);
+
+    // Test with other NULL string
+    result = ft_strjoin(null, test2);
+    printf("Joining NULL and 'World!': %s\n", result); // Your implementation may vary here
+    if (result) free(result);
+
+    // Test with both strings NULL
+    result = ft_strjoin(null, null);
+    printf("Joining NULL and NULL: %s\n", result); // Your implementation may vary here
+    if (result) free(result);
+
+    return 0;
+}*/
--- ft_substr.c
+++ ft_substr.c
@@ -1,9 +1,10 @@
 //HEADER 42
 
 #include <unistd.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
 //#include minha biblioteca
-
-#include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -41,3 +42,43 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 
     return (substr);
 }
+
+/*int main() {
+    char *result;
+    char test_str[] = "Hello, World!";
+
+    // Test 1: Normal case
+    result = ft_substr(test_str, 7, 5);
+    printf("Test 1: %s\n", result);
+    free(result); // Remember to free the allocated memory
+
+    // Test 2: Start index is beyond the end of the string
+    result = ft_substr(test_str, 20, 5);
+    printf("Test 2: %s\n", result);
+    free(result);
+
+    // Test 3: Length goes beyond the end of the string
+    result = ft_substr(test_str, 7, 20);
+    printf("Test 3: %s\n", result);
+    free(result);
+
+    // Test 4: Start at 0, full length
+    result = ft_substr(test_str, 0, strlen(test_str));
+    printf("Test 4: %s\n", result);
+    free(result);
+
+    // Test 5: Empty string
+    result = ft_substr("", 0, 5);
+    printf("Test 5: %s\n", result);
+    free(result);
+
+    // Test 6: Null string
+    result = ft_substr(NULL, 0, 5);
+    if (result == NULL) {
+        printf("Test 6: Passed (NULL string)\n");
+    } else {
+        printf("Test 6: Failed\n");
+    }
+
+    return 0;
+}*/
