2a948ea3b74c0134708664344dddcc92fd6c1228
Amanda Fonteles Da Silveira <afontele@paul-f3Ar7s5.clusters.42paris.fr>
2024-03-22 12:55:02 +0100

    pt3

--- bonus/ft_lstmap.c
+++ bonus/ft_lstmap.c
@@ -4,7 +4,27 @@
 
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+    t_list  *new_list;
+    t_list  *head_new_list;
+
     if (!lst || !f || !del)
         return (NULL);
-    
+    new_list = ft_lstnew(f(lst->content));
+    if (!new_list)
+        return (NULL);
+    head_new_list = new_list;
+    lst = lst->next;
+    while (lst)
+    {
+        new_list->next = ft_lstnew(f(lst->content));
+        if (!new_list->next)
+        {
+            ft_lstclear(&head_new_list, del);
+            return (NULL);
+        }
+        new_list = new_list->next;
+        lst = lst->next;
+    }
+    new_list->next = NULL;
+    return(head_new_list);
 }
